ba1af42994c9ed02c1c554b25271cb6461a9a674
gmlrude <qkrgmlrud00@gmail.com>
2025-02-18 00:02:11 +0900

    81차 2번 문제 다시 풀이

--- live8/test81/문제2/박희경.py
+++ live8/test81/문제2/박희경.py
@@ -1,14 +1,36 @@
 import sys
+from collections import *
 
 input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-network = [[] * (n + 1) for _ in range(n + 1)]
+network = [[] for _ in range(n + 1)]
 
 for _ in range(m):
     x, y = map(int, input().split())
     network[x].append(y)
     network[y].append(x)
 
-# [[], [2, 5], [1, 3, 5], [2], [7], [1, 2, 6], [5], [4]]
+visited = [0] * (n + 1)
+q = deque([1])
+while q:
+    x = q.popleft()
+    visited[x] = 1
+    for nx in network[x]:
+        if not visited[nx]:
+            visited[nx] = 1
+            q.append(nx)
+
+print(sum(visited) - 1)
+
+"""
+7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7
+"""
