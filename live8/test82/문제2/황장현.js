d404a3c84256dba404b56000e0b00dcd3896daf4
Janghyeon Hwang <104212460+eric-hjh@users.noreply.github.com>
2025-02-21 13:59:21 +0900

    82차 2번 문제제

--- live8/test82/문제2/황장현.js
+++ live8/test82/문제2/황장현.js
@@ -10,24 +10,53 @@ const input = fs
 function solution(input) {
   const [N, M] = input[0];
   const relation = input.slice(1);
-  const graph = Array.from({ length: N + 1 }).map((_) =>
-    Array.from({ length: 0 })
-  );
+  const graph = Array.from({ length: N + 1 }, () => []);
 
   for (const [f1, f2] of relation) {
     graph[f1].push(f2);
     graph[f2].push(f1);
   }
 
-  function dfs(start, target) {}
+  function bfs(start, target) {
+    const queue = [[start, 0]];
+    const visited = Array(N + 1).fill(false);
+    visited[start] = true;
+
+    while (queue.length > 0) {
+      const [current, depth] = queue.shift();
+
+      if (current === target) return depth;
+
+      for (let nearNode of graph[current]) {
+        if (!visited[nearNode]) {
+          visited[nearNode] = true;
+          queue.push([nearNode, depth + 1]);
+        }
+      }
+    }
+    return Infinity;
+  }
+
+  let minKevinBacon = Infinity;
+  let minUser = -1;
 
   for (let i = 1; i <= N; i++) {
-    const temp = [];
+    let kevinBacon = 0;
     for (let j = 1; j <= N; j++) {
-      if (graph[i].includes(j)) temp.push(1);
-      else dfs(i, j);
+      if (i !== j) {
+        kevinBacon += bfs(i, j);
+      }
+    }
+
+    if (kevinBacon < minKevinBacon) {
+      minKevinBacon = kevinBacon;
+      minUser = i;
+    } else if (kevinBacon === minKevinBacon) {
+      minUser = Math.min(minUser, i);
     }
   }
+
+  console.log(minUser);
 }
 
-console.log(solution(input));
+solution(input);
