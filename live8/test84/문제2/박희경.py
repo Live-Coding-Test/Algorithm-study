b8113ad4ed589f885f9c327dd0e2815d94bc23cc
gmlrude <qkrgmlrud00@gmail.com>
2025-02-25 01:20:42 +0900

    84차 2번 문제 다시 풀이

--- live8/test84/문제2/박희경.py
+++ live8/test84/문제2/박희경.py
@@ -1,24 +1,19 @@
-"""
-sum(k // result) == n
-이때 최대 result 구하기
-"""
-
 import sys
 
 input = sys.stdin.readline
 
 
-def binary_search(arr):
-    start, end = 1, max(arr)
+def binary_search(cables):
+    start, end = 1, max(cables)
     while start <= end:
         mid = (start + end) // 2  # 반으로 자른 길이
         cnt = 0
-        for i in cm:
-            cnt += i // mid
-        if cnt > n:  # 길이를 늘릴 필요가 있음
-            start = mid + 1
-        else:
+        for cable in cables:
+            cnt += cable // mid
+        if cnt < n:  # 길이를 줄일 필요가 있음
             end = mid - 1
+        else:
+            start = mid + 1     # 놓친 부분) 최대를 구해야 하니까..!
     return end
 
 
@@ -27,7 +22,6 @@ cm = []
 for _ in range(k):
     cm.append(int(input()))
 
-cm.sort()  # [457, 539, 743, 802]
 print(binary_search(cm))
 
 
