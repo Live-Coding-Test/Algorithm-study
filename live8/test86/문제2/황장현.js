a600ced6ecdd567d273f9d2210c119a071ece0a6
Eric <hgh1057@gmail.com>
2025-02-28 11:10:25 +0900

    86차 2번 문제풀이(푸는중)

---
+++ live8/test86/문제2/황장현.js
@@ -0,0 +1,22 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.split(' ').map(Number));
+
+function solution(input) {
+  const N = input[0][0];
+  const 지방예산요청 = input[1].sort((a, b) => a - b);
+  const M = input[2][0];
+
+  let high = 지방예산요청[N - 1];
+  let low = 0;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const sum = 지방예산요청.reduce((acc, v) => acc + (v <= mid ? v : mid), 0);
+  }
+}
+
+console.log(solution(input));
