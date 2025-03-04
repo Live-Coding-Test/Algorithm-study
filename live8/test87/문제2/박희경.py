ccd7889f63965410407fcd98b082d811c265960b
gmlrude <qkrgmlrud00@gmail.com>
2025-03-05 02:03:14 +0900

    87차 2번 문제 다시 풀이

--- live8/test87/문제2/박희경.py
+++ live8/test87/문제2/박희경.py
@@ -3,12 +3,10 @@ def solution(people, limit):
     people.sort()
 
     start, end = 0, len(people) - 1
-    while start < end:
-        mid = (start + end) // 2
-        total = people[start] + people[mid]
-        if total > limit:
-            mid = end - 1
-        else:
-            mid = start + 1
+    while start <= end:
+        if people[start] + people[end] <= limit:
+            start += 1
+        end -= 1
+        answer += 1
 
     return answer
