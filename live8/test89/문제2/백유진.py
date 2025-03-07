a604f93b58d095f7d87f75a9428a13e0d0637b0a
rachel4w2 <rachel4w2@naver.com>
2025-03-07 15:09:31 +0900

    89차 1번, 2번 문제 순서 정정

--- live8/test89/문제2/백유진.py
+++ live8/test89/문제2/백유진.py
@@ -1,24 +1,24 @@
-n = int(input())
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
 
-arr = []
-
-for i in range(n):
-  arr.append(int(input()))
-
-arr.sort()
+answer = 0
 
 start = 0
 end = 0
 
-max = float("-inf")
+cum_sum = arr[start]
 
-while end < n:
-  if arr[end] - arr[start] > 4:
+while end < len(arr):
+  if cum_sum == m:
+    answer += 1
+    cum_sum -= arr[start]
     start += 1
-  else:
-    if end - start + 1 > max:
-      max = end - start + 1
+  elif cum_sum < m:
     end += 1
+    if end < len(arr):
+      cum_sum += arr[end]
+  else:
+    cum_sum -= arr[start]
+    start += 1
 
-
-print(5-max)
+print(answer)
