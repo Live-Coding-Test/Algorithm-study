71a7f0c385b03283f2acacb6d12d2117d4d4fc37
rachel4w2 <rachel4w2@naver.com>
2025-03-17 21:10:47 +0900

    93차 2번 문제풀이

--- live9/test93/문제2/백유진.py
+++ live9/test93/문제2/백유진.py
@@ -1,17 +1,16 @@
 def solution(name):
     answer = 0
     
+    min_cursor = len(name) - 1
     
     for idx, alphabet in enumerate(name):
         answer += min(ord(alphabet) - 65, 91 - ord(alphabet))
-        print(ord(alphabet) - 65)
-        print(91 - ord(alphabet))
         
         end = idx+1
         while end < len(name) and name[end] == 'A':
             end += 1
-        print(idx+1, end)
+        min_cursor = min(min_cursor, idx*2 + len(name)-end, (len(name)-end)*2 + idx)
         
-        answer += min(end-(idx+1), )
-            
+    answer += min_cursor
+    
     return answer
