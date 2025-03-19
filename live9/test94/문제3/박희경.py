3f58e8ef0473a91037b40d8712f95454e6c07695
gmlrude <qkrgmlrud00@gmail.com>
2025-03-19 10:11:38 +0900

    94차 3번 문제 다시 풀이

--- live9/test94/문제3/박희경.py
+++ live9/test94/문제3/박희경.py
@@ -1,19 +1,19 @@
 def solution(stones, k):
     answer = 0
-    
-    i, j = 0, 1
-    while j < len(stones) and j-i <= k:
-        if stones[j] == 0:
-            j += 1
-        i += 1
-        j += 1
-        # for i in range(len(stones)):
-        #     if stones[i] == 0:
-        #         jump += 1
-        #         i += jump - 1
-        #     else:
-        #         stones[i] -= 1
-        answer += 1
-    # print(stones)
-    
+
+    left, right = 1, max(stones)        # 나올 수 있는 stones 원소의 최대값
+    while left <= right:
+        cnt = 0
+        mid = (left + right) // 2       
+        for stone in stones:
+            if stone <= mid:            # mid가 더 크거나 같은 경우 => 0인 경우
+                cnt += 1
+            else:  cnt = 0              # 0 아닌 경우 다시 0 개수 카운팅
+            if cnt >= k: break
+        if cnt < k:                     # mid가 큰 경우가 적었다는 뜻이니 더 크게 만들기
+            left = mid + 1
+        else:
+            answer = mid
+            right = mid - 1
+            
     return answer
