a0f9220417c7677571d5c8ebc2f17c51e92511e7
gmlrude <qkrgmlrud00@gmail.com>
2025-03-20 21:41:47 +0900

    96차 1번 문제 풀이 (참고)

--- live9/test96/문제1/박희경.py
+++ live9/test96/문제1/박희경.py
@@ -3,30 +3,38 @@ from collections import *
 def solution(board):
     answer = 0
     
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    # 상하좌우
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
     
     n = len(board)
     m = len(board[0])
     
-    def bfs(x, y):
-        q = deque([(x, y)])
-        cnt = 0
+    def bfs(x, y, move):
+        q = deque([(x, y, move)])
+        visited = [[0] * m for _ in range(n)]
+        visited[x][y] = 1
+        
         while q:
-            x, y = q.popleft()
-            cnt += 1
+            x, y, move = q.popleft()
+            if board[x][y] == 'G':
+                return move
+            # 'D' 만날 때까지 이동 (이 부분 참고)
             for i in range(4):
-                nx = x + dx[i] 
-                ny = y + dy[i]
-                if 0 <= nx < n or 0 <= y < m or board[x][y] == 'D':
-                    if board[nx][ny] == 'G':
-                        return cnt
-                    q.append((nx, ny))
-            
+                nx, ny = x, y
+                while 0 <= nx + dx[i] < n and 0 <= ny + dy[i] < m and board[nx + dx[i]][ny + dy[i]] != 'D':
+                    nx += dx[i]
+                    ny += dy[i]
+                if not visited[nx][ny]:
+                    visited[nx][ny] = 1
+                    q.append((nx, ny, move + 1))
+        return -1
+    
+
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'R':
-                cnt += bfs(i, j)
-    print(cnt)
+                return bfs(i, j, 0)
+
     
     return answer
