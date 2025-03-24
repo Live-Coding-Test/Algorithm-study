1de225a8b0bdaff2bc21644090cee6c9e98ccf7f
rachel4w2 <rachel4w2@naver.com>
2025-03-24 21:01:43 +0900

    98차 1번 문제풀이

--- live9/test98/문제1/백유진.py
+++ live9/test98/문제1/백유진.py
@@ -7,16 +7,16 @@ dx = [1, 2]
 max = float("-inf")
 
 for p in product(dx, repeat=m):
-  now = 0
+  now = -1
   snow = 1
   for i in p:
     if i == 1:
       now += 1
-      if now < len(arr):
+      if now < n:
         snow += arr[now]
-    if i == 2:
+    elif i == 2:
       now += 2
-      if now < len(arr):
+      if now < n:
         snow //= 2
         snow += arr[now]
   if snow > max:
