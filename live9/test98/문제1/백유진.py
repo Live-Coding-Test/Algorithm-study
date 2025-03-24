0cd45096b80ebcc630112081cf4b10c2402398f3
rachel4w2 <rachel4w2@naver.com>
2025-03-24 20:13:57 +0900

    98차 1번 문제풀이(푸는중)

--- live9/test98/문제1/백유진.py
+++ live9/test98/문제1/백유진.py
@@ -1,2 +1,25 @@
+from itertools import product
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
+
+dx = [1, 2]
+
+max = float("-inf")
+
+for p in product(dx, repeat=m):
+  now = 0
+  snow = 1
+  for i in p:
+    if i == 1:
+      now += 1
+      if now < len(arr):
+        snow += arr[now]
+    if i == 2:
+      now += 2
+      if now < len(arr):
+        snow //= 2
+        snow += arr[now]
+  if snow > max:
+    max = snow
+
+print(max)
