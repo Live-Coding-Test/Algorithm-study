704cbc06a87355df99cf2c2b0e6c9bb1f7b1139c
gmlrude <qkrgmlrud00@gmail.com>
2025-04-02 11:46:00 +0900

    101차 3번 문제 다시 풀이

--- live10/test101/문제3/박희경.py
+++ live10/test101/문제3/박희경.py
@@ -1,7 +1,6 @@
 from collections import * 
 
 def solution(n, edge):
-    answer = 0
     
     graph = [[] for _ in range(n + 1)]
     
@@ -9,17 +8,17 @@ def solution(n, edge):
         graph[a].append(b)
         graph[b].append(a)
     
+    visited = [0] * (n + 1)
+    visited[1] = 1
 
     def bfs(x):
         q = deque([x])
         while q:
             x = q.popleft()
-            print(graph[x])
             for nei in graph[x]:
-                if nei != 0:
-                    nei = 0
-                    answer += 1
+                if not visited[nei]: 
+                    visited[nei] = visited[x] + 1
                     q.append(nei)
     bfs(1)
 
-    return answer
+    return visited.count(max(visited))
