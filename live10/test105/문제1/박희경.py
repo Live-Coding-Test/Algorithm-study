6b1d229b111d7d240f8b7dda31d8110040845c3f
gmlrude <qkrgmlrud00@gmail.com>
2025-04-10 22:55:57 +0900

    105차 1번 문제 다시 풀이

--- live10/test105/문제1/박희경.py
+++ live10/test105/문제1/박희경.py
@@ -1,35 +1,32 @@
 import sys
+from collections import *
 
 input = sys.stdin.readline
 
 
 # 도시 개수, 도로 개수, 거리 정보, 출발 도시
 n, m, k, x = map(int, input().split())
-distance = [[float('inf')] * (n + 1) for _ in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    distance[a][b] = 1
+    graph[a].append(b)
 
-for mid in range(n + 1):
-    for i in range(n + 1):
-        for j in range(n + 1):
-            if i == j: continue
-            if distance[i][mid] != float('inf') and distance[mid][j] != float('inf'):
-                distance[i][j] = min(distance[i][j], distance[i][mid] + distance[mid][j])
+visited = [0] * (n + 1)
+visited[x] = 1
+result = []
+q = deque(([(x, 0)]))
+while q:
+    x, cnt = q.popleft()
+    if cnt == k:
+        result.append(x)
+    for nei in graph[x]:
+        if not visited[nei]:
+            visited[nei] = 1
+            q.append((nei, cnt + 1))
 
-print(distance[x])
-if k not in distance[x]:
+if not result:
     print(-1)
-for dist in distance[x]:
-    if dist == k:
-        print(distance[x].index(dist))
-
-
-"""
-4 4 2 1
-1 2
-1 3
-2 3
-2 4
-"""
+else:
+    for res in sorted(result):
+        print(res)
