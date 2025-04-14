37e864efa4f0ed64dd878397004f1a1cc49d1573
hangyeol <snake011128@naver.com>
2025-04-14 13:13:06 +0900

    106차 2번 문제풀이

--- live10/test106/문제2/백한결.py
+++ live10/test106/문제2/백한결.py
@@ -1,14 +1,35 @@
+import heapq
 from collections import defaultdict
 
 def main():
     n, m = map(int, input().split())
 
-    countryMap = defaultdict(list)
+    cityMap = defaultdict(list)
 
     for _ in range(m):
-        A_i, B_i, C_i = map(int, input().strip().split())
-        countryMap[A_i].append(C_i)
-        countryMap[B_i].append(C_i)
+        a, b, c = map(int, input().split())
+        cityMap[a].append((b, c))
+        cityMap[b].append((a, c))
+
+    dist = [float('inf')] * (n + 1)
+    dist[1] = 0
+
+    pq = []
+    heapq.heappush(pq, (0, 1))
+
+    while pq:
+        cost, now = heapq.heappop(pq)
+
+        if dist[now] < cost:
+            continue
+
+        for next_node, weight in cityMap[now]:
+            new_cost = cost + weight
+            if new_cost < dist[next_node]:
+                dist[next_node] = new_cost
+                heapq.heappush(pq, (new_cost, next_node))
+
+    print(dist[n])
 
 if __name__ == "__main__":
     main()
