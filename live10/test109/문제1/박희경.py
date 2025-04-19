73c4891b896b26acff5ca16997435fa2c22a131d
gmlrude <qkrgmlrud00@gmail.com>
2025-04-20 00:45:03 +0900

    109차 2번 문제풀이(참고)

--- live10/test109/문제1/박희경.py
+++ live10/test109/문제1/박희경.py
@@ -5,15 +5,21 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 
-start, end = 0, 0
-total = a[0]
-cnt = 0
-while start <= end:
-    if total % m == 0:
-        cnt += 1
+remain = [0] * m    # 나머지 개수 
+prefix_sum = 0
+for i in range(n):
+    prefix_sum += a[i]
+    remain[prefix_sum % m] += 1
 
-    total -= a[start]
-    start += 1
-    end += 1
-    if end < n:
-        total += a[end]
+res = remain[0]
+# 나머지가 같은 구간 2개를 뽑으면 나머지가 0이 됨
+for r in remain:
+    # 조합; rC2 = r(r-1) / 2
+    res += r * (r - 1) // 2
+
+print(res)
+
+"""
+5 3
+1 2 3 1 2
+"""
