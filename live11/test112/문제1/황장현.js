aae495a4ec9f34c23d45d397a584b959a7d2c7fc
Eric <hgh1057@gmail.com>
2025-04-25 15:05:44 +0900

    112차 1번 문제풀이(시간초과)

---
+++ live11/test112/문제1/황장현.js
@@ -0,0 +1,48 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.split(' '));
+
+function generateAnagrams(word) {
+  const chars = word.split('').sort();
+  const used = Array(chars.length).fill(false);
+  const result = [];
+  const path = [];
+
+  function backtrack() {
+    if (path.length === chars.length) {
+      console.log(path.join(''));
+      result.push(path.join(''));
+      return;
+    }
+
+    for (let i = 0; i < chars.length; i++) {
+      if (used[i]) continue;
+      if (i > 0 && chars[i] === chars[i - 1] && !used[i - 1]) continue;
+
+      used[i] = true;
+      path.push(chars[i]);
+
+      backtrack();
+
+      path.pop();
+      used[i] = false;
+    }
+  }
+
+  backtrack();
+  return result;
+}
+
+function solution(input) {
+  const N = input[0][0];
+  const words = input.slice(1).map((el) => el[0]);
+
+  for (let i = 0; i < N; i++) {
+    generateAnagrams(words[i]);
+  }
+}
+
+solution(input);
