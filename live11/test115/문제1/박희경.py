3c7f1385cbf9def0d8daab7012e0493a35fd51e3
gmlrude <qkrgmlrud00@gmail.com>
2025-05-05 11:54:57 +0900

    115차 1번 문제풀이(참고)

--- live11/test115/문제1/박희경.py
+++ live11/test115/문제1/박희경.py
@@ -3,14 +3,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a = []
-for _ in range(n):
-    a.append(int(input()))
 
 cnt = 0
-stack = []
-for num in a:
-    if not stack:
-        stack.append(num)
-    else:
-        if num > stack[-1]:
+stack = [int(input())]
+max_num = stack[-1]
+for _ in range(n - 1):
+    num = int(input())
+    if stack[-1] < num:
+        cnt += num - stack[-1]
+        max_num = max(max_num, num)
+    stack.pop()
+    stack.append(num)
+cnt += max_num * len(stack) - sum(stack)
+print(cnt)
