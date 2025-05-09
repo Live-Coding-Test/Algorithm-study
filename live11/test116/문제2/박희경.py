4cdf80e0414c99da292f08bf4699b93d256155eb
gmlrude <qkrgmlrud00@gmail.com>
2025-05-09 12:02:40 +0900

    116차 2번 다시 문제풀이

--- live11/test116/문제2/박희경.py
+++ live11/test116/문제2/박희경.py
@@ -1,10 +1,33 @@
 import sys
+from collections import *
 
 input = sys.stdin.readline
 
 n, t = map(int, input().split())
-hold = []
-for _ in range(n):
-    hold.append(list(map(int, input().split())))
+holds = set()
+y_dict = defaultdict(list)
 
-hold.sort() # [[0, 2], [1, 2], [3, 2], [4, 1], [6, 3]]
+for _ in range(n):
+    x, y = map(int, input().split())
+    holds.add((x, y))
+    y_dict[y].append(x)
+
+visited = set()
+
+q = deque([(0, 0, 0)])
+while q:
+    x, y, cnt = q.popleft()
+    if y == t:
+        print(cnt)
+        break
+
+    for ny in range(y - 2, y + 3):
+        if ny not in y_dict:
+            continue
+        for nx in y_dict[ny]:
+            if abs(nx - x) <= 2 and (nx, ny) in holds and (nx, ny) not in visited:
+                visited.add((nx, ny))
+                q.append((nx, ny, cnt + 1))
+
+else:
+    print(-1)
