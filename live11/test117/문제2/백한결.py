fbed407f3c3ad585bf5cdd4baaaaa2edfa0679f6
hangyeol <snake011128@naver.com>
2025-05-12 13:08:32 +0900

    117차 2번 문제풀이

--- live11/test117/문제2/백한결.py
+++ live11/test117/문제2/백한결.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+sys.setrecursionlimit(10**6)
 
 
 def main():
@@ -11,8 +12,24 @@ def main():
     for _ in range(N-1):
         a, b = map(int, input().split())
         graph[a].append(b)
+        graph[b].append(a)
 
-    print(graph)
+    dp = [[0, 0] for _ in range(N+1)]
+    visited = [False] * (N+1)
+
+    def dfs(node):
+        visited[node] = True
+        dp[node][0] = 0
+        dp[node][1] = 1
+
+        for child in graph[node]:
+            if not visited[child]:
+                dfs(child)
+                dp[node][0] += dp[child][1]
+                dp[node][1] += min(dp[child][0], dp[child][1])
+
+    dfs(1)
+    print(min(dp[1][0], dp[1][1]))
 
 if __name__ == '__main__':
     main()
