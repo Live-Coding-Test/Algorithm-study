410bd30ee30c2f334d2bfd96b86a4aa598e41798
gmlrude <qkrgmlrud00@gmail.com>
2025-05-12 18:07:27 +0900

    117차 2번 다시 풀이

--- live11/test117/문제2/박희경.py
+++ live11/test117/문제2/박희경.py
@@ -1,5 +1,10 @@
+"""
+dp[현재노드] = min(dp[자식노드][얼리어답터인 경우;1], dp[자식노드][아닌 경우;0])
+"""
+
 import sys
 from collections import *
+sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
@@ -8,14 +13,22 @@ n = int(input())
 for _ in range(n-1):
     u, v = map(int, input().split())
     graph[u].append(v)
+    graph[v].append(u)
 
+dp = [[0, 0] for _ in range(n + 1)]     # [노드번호][얼리어답터 체크]
+visited = [0 for _ in range(n + 1)]
+def dfs(start):
+    visited[start] = 1
+    if len(graph[start]) == 0:
+        dp[start][1] = 1
+        dp[start][0] = 0
+    else:
+        for i in graph[start]:
+            if not visited[i]:
+                dfs(i)
+                dp[start][1] += min(dp[i][0], dp[i][1])
+                dp[start][0] += dp[i][1]
+        dp[start][1] += 1
 
-# def parent(x):
-#     x_root = set()
-#     while x in graph[x]:
-#         x_root.add(x)
-#         x = graph[x]
-#     x_root.add(x)
-
-
-# parent(2)
+dfs(1)
+print(min(dp[1][0], dp[1][1]))
