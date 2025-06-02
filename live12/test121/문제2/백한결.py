5a2dbec09868aad59f638031b9afb73e6c14e7be
hangyeol <snake011128@naver.com>
2025-06-02 22:10:16 +0900

    121차 2번 문제풀이

---
+++ live12/test121/문제2/백한결.py
@@ -0,0 +1,13 @@
+import sys
+
+def main():
+    input = sys.stdin.readline
+
+    N = int(input())
+    heights = list(map(int, input().split()))
+
+    result = 0
+
+
+if __name__ == '__main__':
+    main()
