edd4cc8e799dbd8874358f99be351e3ab30d371f
gmlrude <qkrgmlrud00@gmail.com>
2025-06-09 09:46:57 +0900

    122차 문제 순서 변경

--- live12/test122/문제2/박희경.py
+++ live12/test122/문제2/박희경.py
@@ -2,19 +2,30 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-k = int(input())
-sensor = list(map(int, input().split()))
-sensor.sort()
+n, m = map(int, input().split())
+area = []
+for _ in range(n):
+    area.append(list(map(int, input().split())))
 
-res, cnt = 0, 1
-for i in range(n):
-    if i % 2 != 0:
-        if i == n - 1: abs(sensor[i] - sensor[i-1])
-        else:
-            res += min(abs(sensor[i] - sensor[i-1]), abs(sensor[i+1] - sensor[i]))
-    else:
-        if cnt < k:
-            cnt += 1
-    
-print(res)
+prefix_sum = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + area[i-1][j-1]
+
+k = int(input())
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = prefix_sum[x2][y2] - prefix_sum[x1 - 1][y2] - prefix_sum[x2][y1 - 1] + prefix_sum[x1-1][y1-1]
+    print(result)
+
+"""
+4 4
+9 14 29 7
+1 31 6 13
+21 26 40 16
+8 38 11 23
+3
+1 1 3 2
+1 1 1 4
+1 1 4 4
+"""
